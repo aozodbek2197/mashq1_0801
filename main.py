47ccee4567bf4494b9882a7b4b4750644aa50586
aozodbek2197 <aozodbek2197@gmail.com>
2026-01-08 13:36:09 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# # 1-misol
+# royxat = ["hello", "say", "hi", "welcome"]
+
+# natija = list(map(lambda x: f"{x[0]}-{len(x)}", royxat))
+# print(natija)
+# # 2-misol
+# royxat = ["Ali Valiyev", "Bobur Yo'ldoyev", "Stive Jobs", "Bill Gates"]
+
+# natija = list(map(lambda x: x.split()[0] + " " + x.split()[1][0] + ".", royxat))
+# print(natija)
+# # 3-misol
+# royxat = ["Ali", "Bobur", "Stive", "Santyago"]
+
+# natija = list(map(lambda x: x + str(len(x)), royxat))
+# print(natija)
+# # 4-misol
+# sozlar = ["HELLo", "saY", "hi", "Welcome"]
+
+# natija = list(map(lambda x: x[:-1] + x[-1].upper(), sozlar))
+# print(natija)
